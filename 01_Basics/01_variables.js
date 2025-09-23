4e932eeeb6557c8052a58757793a32a8b6be8cfd
Aaradhana  Mewade <117636129+aaradhana1712@users.noreply.github.com>
2025-09-23 22:04:08 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "aaru@google.com";
+var accountPassword = "12342";
+accountCity = "Indore";
+let accountState;
+
+
+//accountId = 2;
+
+
+accountEmail = "aa@c.com";
+accountPassword = "1121";
+accountCity = "Noida";
+
+
+console.log(accountId);
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
