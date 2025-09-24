303ec49d98c7c2de5a85b3ebf5d94747ab9bb8f5
Aaradhana  Mewade <117636129+aaradhana1712@users.noreply.github.com>
2025-09-24 00:05:46 +0000

    dataType summary

---
+++ 01_Basics/dataTypes summary.js
@@ -0,0 +1,26 @@
+// primitive
+// 7 tpres : Strring,Number,Bollean,null, undefined, symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+
+const id = symbol('123');
+const anotherId = symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 121211311313n
+
+
+
+// reference (Non primitive)
+
+// Array, Object, Functions
+
+
+
